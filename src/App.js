aa784995fe0ad09d1b47109a796595d088f15ca1
Alexander <lenchenkov.a.d@gmail.com>
2023-03-27 04:25:57 +0500

    complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
+import { useState } from 'react';
 import './index.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
-      </div>
-    </div>
-  );
+	const [counter, setCounter] = useState(0);
+
+	const onClickPlus = () => {
+		if (counter < 10) {
+			setCounter(counter + 1);
+		}
+	};
+	const onClickMinus = () => {
+		if (counter > 0) {
+			setCounter(counter - 1);
+		}
+	};
+
+	return (
+		<div className="App">
+			<div>
+				<h2>Счетчик:</h2>
+				<h1>{counter}</h1>
+				<button onClick={onClickMinus} className="minus">
+					- Минус
+				</button>
+				<button onClick={onClickPlus} className="plus">
+					Плюс +
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
